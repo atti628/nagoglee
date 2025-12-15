83343fbfaf2f5a11dc765934e775dd5ba5382564
Sota Achiha <sota6.28sota@icloud.com>
2025-12-15 15:26:48 +0900

    第一弾完成公開3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import sitemap from '@astrojs/sitemap'; // ★ここを追加
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://nagoglee.pages.dev',
+  
+  // ★ここを追加（これがサイトマップを作ってくれます）
+  integrations: [
+    sitemap()
+  ],
+
   vite: {
     plugins: [tailwindcss()]
   }
